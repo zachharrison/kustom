773deadf3f78e428339335bc2c93fc4cc17ab1bc
Zach Harrison <zharrison@shaw.ca>
2021-03-13 12:20:49 -0800

    Refactored commonjs requires to use es6 modules in server.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -65,4 +65,4 @@ const products = [
   },
 ];
 
-module.exports = products;
+export default products;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
