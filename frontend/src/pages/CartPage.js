98dc84560aeeee63145a99ba4ebae082f5fc39ea
Zach Harrison <zharrison@shaw.ca>
2021-03-14 13:43:16 -0700

    Added some jsx to display cart items in CartPage component

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   Row,
@@ -30,8 +30,48 @@ const CartPage = ({ match, location, history }) => {
     }
   }, [dispatch, productId, size]);
 
-  console.log(size);
-  return <div>Cart</div>;
+  const removeFromCartHandler = (id) => console.log('remove');
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'> Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/products/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>Size: {item.size.toUpperCase()}</Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash' />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={2}></Col>
+      <Col md={2}></Col>
+    </Row>
+  );
 };
 
 export default CartPage;
