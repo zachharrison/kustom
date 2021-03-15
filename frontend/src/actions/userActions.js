9eb3950e0fa0fcadce80b633f245f86ef3420ff2
Zach Harrison <zharrison@shaw.ca>
2021-03-15 16:37:12 -0700

    Added reducer and action to update a users profile details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -10,6 +10,9 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -51,6 +54,7 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch({ type: USER_LOGIN_LOGOUT });
+  document.location.href = '/login';
 };
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -121,6 +125,41 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     // CHECK TO SEE IF THERE IS A CUSTOM ERROR MESSAGE FROM THE SERVER, IF NOT JUST RETURN THE DEFAULT
     dispatch({
       type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(`/api/users/profile`, user, config);
+
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    // CHECK TO SEE IF THERE IS A CUSTOM ERROR MESSAGE FROM THE SERVER, IF NOT JUST RETURN THE DEFAULT
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
