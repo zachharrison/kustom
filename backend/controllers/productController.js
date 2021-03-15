64ee5f6945f65b0827004f9f40cc9225946ebae5
Zach Harrison <zharrison@shaw.ca>
2021-03-14 19:23:22 -0700

    Refatored productRoutes to point to the newly created productController file for the functionality

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+// @DESC      FETCH ALL PRODUCTS
+// @ROUTE     GET /api/products
+// @ACCESS    PUBLIC
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @DESC      FETCH A SINGLE PRODUCT
+// @ROUTE     GET /api/products/:id
+// @ACCESS    PUBLIC
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProducts, getProductById };
