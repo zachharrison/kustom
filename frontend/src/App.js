4214c625f2b2dead412dd5feda50b84fc14d6ebe
Zach Harrison <zharrison@shaw.ca>
2021-03-15 20:49:20 -0700

    Created the PaymentPage component and added the action and reducer to save the payment method for a user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import ShippingPage from './pages/ShippingPage';
+import PaymentPage from './pages/PaymentPage';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Container>
           <Route path='/login' component={LoginPage} />
           <Route path='/shipping' component={ShippingPage} />
+          <Route path='/payment' component={PaymentPage} />
           <Route path='/register' component={RegisterPage} />
           <Route path='/profile' component={ProfilePage} />
           <Route path='/product/:id' component={ProductPage} />
