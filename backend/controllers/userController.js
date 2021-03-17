26b30565dd88491baf95b2e3efd076e24adaf0af
Zach Harrison <zharrison@shaw.ca>
2021-03-17 12:41:58 -0700

    Added the functionality and JSX for the admins to delete users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -116,4 +116,26 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
+// @DESC      DELETE A USER
+// @ROUTE     DELETE /api/users/:id
+// @ACCESS    PRIVATE/ADMIN
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.remove();
+    res.json({ message: 'User deleted' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+  res.json(users);
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+};
