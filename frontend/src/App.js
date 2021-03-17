a33023befb9d9b7df8c60eeef87d90c93f473ce7
Zach Harrison <zharrison@shaw.ca>
2021-03-17 11:47:44 -0700

    Added the UserListPage, which shows a link if the logged in user is an admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingPage from './pages/ShippingPage';
 import PaymentPage from './pages/PaymentPage';
 import PlaceOrderPage from './pages/PlaceOrderPage';
 import OrderPage from './pages/OrderPage';
+import UserListPage from './pages/UserListPage';
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
           <Route path='/profile' component={ProfilePage} />
           <Route path='/product/:id' component={ProductPage} />
           <Route path='/cart/:id?' component={CartPage} />
+          <Route path='/admin/userlist' component={UserListPage} />
           <Route path='/' component={Home} exact />
         </Container>
       </main>
