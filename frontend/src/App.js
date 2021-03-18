a183ee74f385b8a1c54354fde238e4e5aec95927
Zach Harrison <zharrison@shaw.ca>
2021-03-18 09:43:36 -0700

    Created the admin ProductListPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderPage from './pages/PlaceOrderPage';
 import OrderPage from './pages/OrderPage';
 import UserListPage from './pages/UserListPage';
 import UserEditPage from './pages/UserEditPage';
+import ProductListPage from './pages/ProductListPage';
 
 const App = () => {
   return (
@@ -33,6 +34,7 @@ const App = () => {
           <Route path='/cart/:id?' component={CartPage} />
           <Route path='/admin/userlist' component={UserListPage} />
           <Route path='/admin/user/:id/edit' component={UserEditPage} />
+          <Route path='/admin/productlist' component={ProductListPage} />
           <Route path='/' component={Home} exact />
         </Container>
       </main>
