4c6131b1c34c3fcee2923d273981a523d74bc8bc
Zach Harrison <zharrison@shaw.ca>
2021-03-18 13:14:47 -0700

    Created routes and controllers for edit and delete products and tested them in postman

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -40,4 +40,79 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+// @DESC      CREATE A PRODUCT
+// @ROUTE     POST /api/products
+// @ACCESS    PRIVATE/ADMIN
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    category: 'sample',
+    numReviews: 0,
+    size: {
+      small: 0,
+      medium: 1,
+      large: 5,
+      xlarge: 4,
+    },
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+
+  res.status(201).json(createdProduct);
+});
+
+// @DESC      UPDATE A PRODUCT
+// @ROUTE     PUT /api/products/:id
+// @ACCESS    PRIVATE/ADMIN
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    category,
+    size: { small, medium, large, xlarge },
+    numReviews,
+    rating,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.size = {
+      small,
+      medium,
+      large,
+      xlarge,
+    };
+    product.name = name;
+    product.price = price;
+    product.decription = description;
+    product.image = image;
+    product.category = category;
+    product.numReviews = numReviews;
+    product.rating = rating;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+  const createdProduct = await product.save();
+
+  res.status(201).json(product);
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
