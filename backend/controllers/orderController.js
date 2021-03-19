788706003e557cff89680efe651d6756b6a05c3a
Zach Harrison <zharrison@shaw.ca>
2021-03-19 16:54:30 -0700

    Added route for admins to mark an order as delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -96,10 +96,30 @@ const getOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
+// @DESC      UPDATE ORDER TO DELIVERED
+// @ROUTE     PUT /api/orders/:id/deliver
+// @ACCESS    PRIVATE/ADMIN
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
   getOrders,
+  updateOrderToDelivered,
 };
