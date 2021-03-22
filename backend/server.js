56a1316601f396a1a4337e93d31093d85263f78f
Zach Harrison <zharrison@shaw.ca>
2021-03-22 16:29:20 -0700

    Prepare for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 // ROUTES
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -40,6 +36,18 @@ app.get('/api/config/paypal', (req, res) =>
 // CREATED CUSTOM DIRNAME VARIABLE BECAUSE IT IS NOT AVAILABLE WITH ES6 MODULES
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 // CUSTOM MIDDLEWARE FOR ERROR HANDLING
 app.use(notFound);
